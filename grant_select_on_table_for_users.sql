7828cdc24ace5234f3c16c70a0b045b415c00c00
Michael Xia <chilmix@amazon.com>
2017-04-14 17:31:05 -0400

    grant table select
    grant table select snippet

---
+++ grant_select_on_table_for_users.sql
@@ -0,0 +1,5 @@
+
+GRANT SELECT ON TABLE public.dim_iso_week  user1, user2, user3;
+GRANT SELECT ON public.dim_iso_week TO group user_group_for_select;
+GRANT SELECT ON public.dim_iso_week TO user_one;
+
